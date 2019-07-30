7db3cf879dfdc2bb2c7ec5e61e22dcdddedf48c0
Tom McCulloch <tommcculloch@Toms-Macbook-Air.local>
2019-07-31 06:41:43 +1200

    Adding skeleton of cost_function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use rand::Rng;
+
+
 fn main() {
-    println!("Hello, world!");
+    let x = [3,4,7,8,9,13];
+    let y = [5,8,9,11,13,15];
+    let i : f32 = rand::thread_rng().gen_range(1.0f32, 15.0f32);
+    let c : f32 = rand::thread_rng().gen_range(1.0f32, 15.0f32);
+
+    cost_function(&x, &y, i, c);
+    print!("1");
+}
+
+fn cost_function(x: &[i32], y: &[i32], i: f32, c: f32) -> f32{
+
+
+    
+    1.0f32
 }
