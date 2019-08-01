af77cecdfdecbc9785b19752565570427d16e88e
Tom McCulloch <tommcculloch@Toms-Macbook-Air.local>
2019-08-02 06:28:43 +1200

    First attempt at cost funciton

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,15 @@ fn main() {
     let i : f32 = rand::thread_rng().gen_range(1.0f32, 15.0f32);
     let c : f32 = rand::thread_rng().gen_range(1.0f32, 15.0f32);
 
-    cost_function(&x, &y, i, c);
-    print!("1");
+    println!("Cost is {}", cost_function(&x, &y, i, c));
 }
 
 fn cost_function(x: &[i32], y: &[i32], i: f32, c: f32) -> f32{
 
+    let mut total_error = 0.0f32;
+    for n in 0..x.len(){
+        total_error += (y[n] as f32) - (i * (x[n] as f32) + c);
+    }
 
-    
-    1.0f32
+    total_error / x.len() as f32
 }
