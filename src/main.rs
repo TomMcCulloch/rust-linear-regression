5e4d81e423ac60b3a8bc6717c4e96504c25f9745
Tom McCulloch <tommcculloch@Toms-Macbook-Air.local>
2019-08-05 06:58:22 +1200

    Adding update weights function

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,35 @@ use rand::Rng;
 
 
 fn main() {
-    let x = [3,4,7,8,9,13];
-    let y = [5,8,9,11,13,15];
+    let x : [f32; 6] = [3.0,4.0,7.0,8.0,9.0,13.0];
+    let y : [f32; 6] = [5.0,8.0,9.0,11.0,13.0,15.0];
     let i : f32 = rand::thread_rng().gen_range(1.0f32, 15.0f32);
     let c : f32 = rand::thread_rng().gen_range(1.0f32, 15.0f32);
 
     println!("Cost is {}", cost_function(&x, &y, i, c));
 }
 
-fn cost_function(x: &[i32], y: &[i32], i: f32, c: f32) -> f32{
+fn cost_function(x: &[f32], y: &[f32], i: f32, c: f32) -> f32{
 
     let mut total_error = 0.0f32;
     for n in 0..x.len(){
-        total_error += (y[n] as f32) - (i * (x[n] as f32) + c);
+        total_error += y[n] - (i * x[n] + c);
     }
 
     total_error / x.len() as f32
+}
+
+fn update_weights(x: &[f32], y: &[f32], i: f32, c: f32, learning_rate: i32) -> (i32, i32){
+
+    let i_deriv = 0;
+    let c_deriv = 0;
+
+    for n in 0..x.len(){
+        i_deriv += -2*x[n] * (y[n] - (i*x[n] + c));
+
+        # -2(y - (mx + b))
+        bias_deriv += -2*(sales[i] - (weight*radio[i] + bias))
+    }
+
+    (1 , 2)
 }
