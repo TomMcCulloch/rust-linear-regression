1175f6593c551ca93469bda90949ed99a8d7bb70
Tom McCulloch <tommcculloch@Toms-Macbook-Air.local>
2019-08-07 06:46:18 +1200

    Tweaked update weights function

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,12 @@ use rand::Rng;
 
 
 fn main() {
-    let x : [f32; 6] = [3.0,4.0,7.0,8.0,9.0,13.0];
-    let y : [f32; 6] = [5.0,8.0,9.0,11.0,13.0,15.0];
+    let x : [f32; 10] = [3.0,4.0,7.0,8.0,9.0,13.0,13.5,14.1,18.9,19.2];
+    let y : [f32; 10] = [5.0,8.0,9.0,11.0,13.0,15.0,15.2,17.2,21.0,22.1];
     let i : f32 = rand::thread_rng().gen_range(1.0f32, 15.0f32);
     let c : f32 = rand::thread_rng().gen_range(1.0f32, 15.0f32);
 
-    train(&x, &y, i, c, 0.1, 10);
+    train(&x, &y, i, c, 0.001, 10);
 }
 
 fn cost_function(x: &[f32], y: &[f32], i: f32, c: f32) -> f32{
@@ -24,6 +24,7 @@ fn update_weights(x: &[f32], y: &[f32], i: f32, c: f32, learning_rate: f32) -> (
 
     let mut i_deriv : f32 = 0.0;
     let mut c_deriv : f32 = 0.0;
+    println!("  - InputW: {}, InputB: {}, i_deriv: {}, c_deriv: {}", i, c, i_deriv, c_deriv);
 
     for n in 0..x.len(){
         i_deriv += -2.0*x[n] * (y[n] - (i*x[n] + c));
@@ -32,7 +33,11 @@ fn update_weights(x: &[f32], y: &[f32], i: f32, c: f32, learning_rate: f32) -> (
     }
     println!("  - InputW: {}, InputB: {}, i_deriv: {}, c_deriv: {}", i, c, i_deriv, c_deriv);
 
-    (i - (i_deriv / (x.len() as f32) * learning_rate), c - (c_deriv / (x.len() as f32) * learning_rate))
+    i_deriv = i -(i_deriv / (x.len() as f32)) * learning_rate;
+    c_deriv = c - (c_deriv / (x.len() as f32)) * learning_rate;
+    println!("returning i: {}, c: {}", i_deriv, c_deriv);
+
+    (i_deriv,c_deriv)
 }
 
 fn train(x: &[f32], y: &[f32], i: f32, c: f32, learning_rate: f32, iters: i32) -> (f32, f32, Vec<f32>) {
@@ -41,10 +46,8 @@ fn train(x: &[f32], y: &[f32], i: f32, c: f32, learning_rate: f32, iters: i32) -
     let mut weight = i;
     let mut bias = c;
 
-    let mut vals = (weight, bias);
-
     for i in 1..iters {
-        vals = update_weights(x, y, weight, bias, learning_rate);
+        let vals = update_weights(x, y, weight, bias, learning_rate);
         weight = vals.0;
         bias = vals.1;
         let cost: f32 = cost_function(x, y, weight, bias);
